753ae2394fce32633f629bf874ca37ef9135bfa5
Eddy Elguezabal <eddye0820@gmail.com>
2020-10-25 22:46:13 -0400

    Challenge commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -8,12 +8,25 @@ public class PlayerScript : MonoBehaviour
     private Rigidbody2D rd2d;
     public float speed;
     public Text score;
+    public Text winText;
+    public Text livesText;
     private int scoreValue = 0;
+    private int livesValue = 0;
+    private SpriteRenderer spriteRenderer;
+    public AudioClip music;
+    public AudioClip victorySound;
+    public AudioSource musicSource;
 
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        livesValue = 3;
+        SetLivesText();
         score.text = scoreValue.ToString();
+        winText.text = "";
+        musicSource.clip = music;
+        musicSource.Play();
     }
 
     void FixedUpdate()
@@ -36,6 +49,27 @@ public class PlayerScript : MonoBehaviour
             scoreValue += 1;
             score.text = scoreValue.ToString();
             Destroy(collision.collider.gameObject);
+
+            if(scoreValue == 4)
+            {
+                transform.position = new Vector2(82.0f, 0.12f);
+                livesValue = 3;
+                SetLivesText();
+            }
+            else if(scoreValue == 8)
+            {
+                musicSource.Stop();
+                musicSource.clip = victorySound;
+                musicSource.Play();
+                winText.text = "You win! Game created by Eddy Elguezabal.";
+            }
+        }
+
+        if(collision.collider.tag == "Enemy")
+        {   
+            livesValue = livesValue - 1;
+            SetLivesText2(collision);
+            Destroy(collision.collider.gameObject);
         }
     }
 
@@ -43,10 +77,26 @@ public class PlayerScript : MonoBehaviour
     {
         if(collision.collider.tag == "Ground")
         {
-            if(Input.GetKey(KeyCode.W))
+            if(Input.GetKeyDown(KeyCode.W))
             {
-                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+                rd2d.AddForce(new Vector2(0, 8), ForceMode2D.Impulse);
             }
+        }
+    }
+
+    void SetLivesText()
+    {
+        livesText.text = "Lives: " + livesValue.ToString();
+    }
+
+    void SetLivesText2(Collision2D collision)
+    {
+        livesText.text = "Lives: " + livesValue.ToString();
+        if (livesValue == 0)
+        {
+            winText.text = "You Lose!";
+            spriteRenderer.enabled = false;
+            rd2d.isKinematic = true;
         }
     }
 }
